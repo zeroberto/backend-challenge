5e3161ccefa8ca59af4920b5685da70ac2a22235
zeroberto <roberto.jrnj@gmail.com>
2019-02-21 12:28:24 -0300

    Definition of configuration class for basic application settings.

---
+++ src/main/java/com/invillia/acme/config/DefaultConfig.java
@@ -0,0 +1,50 @@
+package com.invillia.acme.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import static com.invillia.acme.constants.ConfigConstants.STRENGTH;
+
+/**
+ * Class containing the basic configuration settings.
+ *
+ * @author José Roberto <roberto.jrnj@gmail.com>
+ */
+@Configuration
+public class DefaultConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(STRENGTH);
+    }
+
+}
