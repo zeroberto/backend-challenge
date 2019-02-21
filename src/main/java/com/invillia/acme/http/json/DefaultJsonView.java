4e0164fc9221c1c47092790ca1b9073742dbc94f
zeroberto <roberto.jrnj@gmail.com>
2019-02-21 11:58:52 -0300

    Adding javadoc to group interfaces.

--- src/main/java/com/invillia/acme/http/json/DefaultJsonView.java
+++ src/main/java/com/invillia/acme/http/json/DefaultJsonView.java
@@ -6,18 +6,35 @@ package com.invillia.acme.http.json;
  * @author José Roberto <roberto.jrnj@gmail.com>
  */
 public interface DefaultJsonView {
+
+    /**
+     * Interface representing the object rendering group for requests.
+     */
     interface RequestJsonView extends DefaultJsonView {
     }
 
+    /**
+     * Interface that represents the object rendering group for responses.
+     */
     interface ResponseJsonView extends DefaultJsonView {
     }
 
+    /**
+     * Interface that represents the object rendering group for PUT requests.
+     */
     interface PutRequestJsonView extends DefaultJsonView {
     }
 
+    /**
+     * Interface representing the object rendering group for PATCH requests.
+     */
     interface PatchRequestJsonView extends DefaultJsonView {
     }
 
+    /**
+     * Interface that represents the rendering group of objects that are
+     * subtypes of others.
+     */
     interface SubtypeResponseJsonView extends DefaultJsonView {
     }
 }
--- src/main/java/com/invillia/acme/validation/DefaultValidationGroup.java
+++ src/main/java/com/invillia/acme/validation/DefaultValidationGroup.java
@@ -2,25 +2,59 @@ package com.invillia.acme.validation;
 
 import com.invillia.acme.http.json.DefaultJsonView;
 
+/**
+ * Class containing the application validation groups for using javax.validation.
+ *
+ * @author José Roberto <roberto.jrnj@gmail.com>
+ */
 public interface DefaultValidationGroup {
+
+    /**
+     * Interface that represents the specific validation for the creation
+     * of the validated object.
+     */
     interface CreateValidationGroup extends RequestValidationGroup {
     }
 
+    /**
+     * Interface that represents the specific validation for the update
+     * of the validated object.
+     */
     interface UpdateValidationGroup extends DefaultValidationGroup {
     }
 
+    /**
+     * Interface that represents the specific validation for the partial update
+     * of the validated object.
+     */
     interface PartialUpdateValidationGroup extends DefaultValidationGroup {
     }
 
+    /**
+     * Interface that represents the specific validation for requests that modify
+     * the validated object in some way.
+     */
     interface RequestValidationGroup extends DefaultJsonView {
     }
 
+    /**
+     * Interface that represents the specific validation for requests that require the presence
+     * of the sub-elements of the validated object.
+     */
     interface SupertypeRequiredValidationGroup extends DefaultValidationGroup {
     }
 
+    /**
+     * Interface that represents the specific validation for the payment
+     * of the validated object.
+     */
     interface PaymentRequiredValidationGroup extends DefaultValidationGroup {
     }
 
+    /**
+     * Interface that represents the specific validation for the order items
+     * of the validated object.
+     */
     interface OrderItemsRequiredValidationGroup extends DefaultValidationGroup {
     }
 }
